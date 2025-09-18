7e18f7896466ba30bb6d0ffa5e44f9d59eae5fb9
kboy101222 <george.kyle87@gmail.com>
2025-09-17 20:05:36 -0500

    changed base again

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ void main() {
   // [Document] renders the root document structure (<html>, <head> and <body>)
   // with the provided parameters and components.
   runApp(Document(
-    base: 'middletncrawlspace.com',
+    // base: 'middletncrawlspace.com',
     title: 'Middle TN Crawlspace',
     head: [
       link(href: 'styles.css', rel: 'stylesheet'),
